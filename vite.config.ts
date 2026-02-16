21fb3ca6e063e1933e250913cbd57e44ea1ad07f
Nhat0301 <96116589+Nhat0301@users.noreply.github.com>
2026-02-16 20:33:03 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+        base: '/test/',
       server: {
-          base: '/test/',
         port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react()],
-
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
